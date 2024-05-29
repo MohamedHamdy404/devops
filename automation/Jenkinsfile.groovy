17011eb3fba2c4d0393499a1a656e4b13f171828
= <=>
2024-05-29 15:03:06 +0300

    test

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -2,20 +2,7 @@ pipeline {
     agent any
 
     stages {
-        stage('Checkout') {
-            steps {
-                // Checkout the repository with submodules
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: '*/main']],
-                    doGenerateSubmoduleConfigurations: false,
-                    extensions: [
-                        [$class: 'SubmoduleOption', recursiveSubmodules: true, trackingSubmodules: true]
-                    ],
-                    userRemoteConfigs: [[url: 'https://github.com/MohamedHamdy404/repo.git']]
-                ])
-            }
-        }
+
         stage('Load Pipeline from Submodule') {
             steps {
                 script {
